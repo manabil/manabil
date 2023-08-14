d32189abda15500101f8933c7d3d752564f0be5d
Muhammad Ammar Nabil <mammarnabil@students.unnes.ac.id>
2023-08-14 17:24:00 +0700

    Update myLifetime.py

--- myLifetime.py
+++ myLifetime.py
@@ -1,80 +1,74 @@
 import datetime
 # from dateutil import relativedelta
 
-def getDays(date) -> str:
-    return date.strftime('%a')
+def get_days(date) -> str:
+    return date.strftime('%a');
 
-def isIndependenceDay():
+def is_independence_day() -> bool:
     id_independences = datetime.date(1945, 8, 17)
     if(id_independences.day == now_date.day and id_independences.month == now_date.month):
-        return True
-    return False
+        return True;
+    return False;
 
-def isKartiniDay():
-    kartiniDays = datetime.date(1879, 4, 21)
-    if(kartiniDays.day == now_date.day and kartiniDays.month == now_date.month):
-        return True
-    return False
+def is_kartini_day() -> bool:
+    kartini_days = datetime.date(1879, 4, 21)
+    if(kartini_days.day == now_date.day and kartini_days.month == now_date.month):
+        return True;
+    return False;
 
-def isYouthPledge():
-    YouthPledgeDays = datetime.date(1928, 10, 28)
-    if(YouthPledgeDays.day == now_date.day and YouthPledgeDays.month == now_date.month):
-        return True
-    return False
+def is_youth_pledge() -> bool:
+    youth_pledge_days = datetime.date(1928, 10, 28)
+    if(youth_pledge_days.day == now_date.day and youth_pledge_days.month == now_date.month):
+        return True;
+    return False;
 
-def isThirtiethSept():
-    ThirtiethSeptDays = datetime.date(1965, 9, 30)
-    if(ThirtiethSeptDays.day == now_date.day and ThirtiethSeptDays.month == now_date.month):
-        return True
-    return False
+def is_thirty_sept() -> bool:
+    thirty_sept_days = datetime.date(1965, 9, 30)
+    if(thirty_sept_days.day == now_date.day and thirty_sept_days.month == now_date.month):
+        return True;
+    return False;
 
-def isBirthday():
-    if(my_date.day == now_date.day and my_date.month == now_date.month):
-        return True
-    return False
+def weekDays() -> str:
+    if(get_days(now_date) == 'Mon'):
+        return "Why's monday so far from friday ? 👀";
+    elif(get_days(now_date) == 'Tue'):
+        return "It's just a second monday 😪";
+    elif(get_days(now_date) == 'Wed'):
+        return "Halfway through the week 🙃";
+    elif(get_days(now_date) == 'Thu'):
+        return "Yeay tomorrow is friday 😬";
+    elif(get_days(now_date) == 'Fri'):
+        return "We made it, It's finally friday 🥳";
+    elif(get_days(now_date) == 'Sat'):
+        return "Ahh, It's the weekend 😎";
+    else:
+        return "Tomorrow is monday again 😌";
 
-def generateMarkdown():
-    lines = []
+def generateMarkdown() -> None:
+    lines: list = [];
 
     with open('README.md', 'r', encoding='utf-8') as file:
         lines = file.readlines()
-        lines[17] = f"- ⏳ I've been alive for {(now_date - my_date).days} days\n"
-        if isBirthday():
-            lines[132] = f'<h5><i>"It is my birthday ? Ah i forgot it 🙂"</i></h5>'
-        elif isIndependenceDay():
-            lines[132] = f'<h5><i>"Happy Independece Indonesia 🇮🇩🔥"</i></h5>'
-        elif isKartiniDay():
-            lines[132] = f'<h5><i>"Happy Kartini Day 🇮🇩👵"</i></h5>'
-        elif isYouthPledge():
-            lines[132] = f'<h5><i>"Happy Youth Pledge Day 🇮🇩✊👊"</i></h5>'
-        elif isThirtiethSept():
-            lines[132] = f'<h5>"Happy Independece Day 🇮🇩🛡🔫"</h5>'
+        lines[17] = f"- 🛌 I've spent {round(((now_date-random_date).days * 7)/24)} days of my life asleep\n"
+        if is_independence_day():
+            lines[132] = f'<h5><i>"Happy Independece Indonesia 🇮🇩🔥"</i></h5>';
+        elif is_kartini_day():
+            lines[132] = f'<h5><i>"Happy Kartini Day 🇮🇩👵"</i></h5>';
+        elif is_youth_pledge():
+            lines[132] = f'<h5><i>"Happy Youth Pledge Day 🇮🇩✊👊"</i></h5>';
+        elif is_thirty_sept():
+            lines[132] = f'<h5>"Happy Independence Day 🇮🇩🛡🔫"</h5>';
         else:
-            lines[132] = f'<h5><i>"{weekDays()}"</i></h5>\n'
-            
+            lines[132] = f'<h5><i>"{weekDays()}"</i></h5>\n';
 
     with open('README.md', 'w', encoding='utf-8') as file:
-        file.writelines(lines)
+        file.writelines(lines);
 
-def weekDays():
-    if(getDays(now_date) == 'Mon'):
-        return "Why's monday so far from friday ? 👀"
-    elif(getDays(now_date) == 'Tue'):
-        return "It's just a second monday 😪"
-    elif(getDays(now_date) == 'Wed'):
-        return "Halfway through the week 🙃"
-    elif(getDays(now_date) == 'Thu'):
-        return "Yeay tomorrow is friday 😬"
-    elif(getDays(now_date) == 'Fri'):
-        return "We made it, It's finally friday 🥳"
-    elif(getDays(now_date) == 'Sat'):
-        return "Ahh, It's the weekend 😎"
-    elif(getDays(now_date) == 'Sun'):
-        return "Tomorrow is monday again 😌"
 
-now_date = datetime.datetime.now()
-my_unixtime_stamp = 1000405800
-my_date = datetime.datetime.fromtimestamp(my_unixtime_stamp)
+
+now_date: datetime.datetime = datetime.datetime.now();
+random_number: int = 1000405800;
+random_date: datetime.datetime = datetime.datetime.fromtimestamp(random_number);
 # delta_time = relativedelta.relativedelta(now_date, my_date)
 
-generateMarkdown()
+generateMarkdown();
